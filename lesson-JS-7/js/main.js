d4219dd1bc7997ba48f1f2a7988f739d0b5b0e80
Denis <68554683+Gorichan@users.noreply.github.com>
2022-03-03 13:30:05 +0300

    Add files via upload

---
+++ lesson-JS-7/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let time = prompt('What time is it?');
+
+let sumTime = time * 3600;
+
+let sumTimeSecond = sumTime;
+
+ alert( sumTimeSecond + ' seconds');
---
+++ lesson-JS-8/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let num = 1;
+num = num += 12;
+num = num -= 14;
+num = num *= 5;
+num = num /= 7;
+num = num ++, 1;
+num = num --, 1;
+alert(num);
